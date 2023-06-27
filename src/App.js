a15703c7ea5fe83f7e3a6528f756758c85d7c7dd
its-mohd-kaif <mohdkaif@cedcoss.com>
2023-06-27 15:57:15 +0530

    Test Commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [count, setCount] = useState(0);
 
   const increase = () => {
-    setCount((prevCount) => prevCount + 1);
+    setCount((prevCount) => prevCount + 1)
   };
 
   const decrease = () => {
