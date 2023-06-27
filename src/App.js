8d8dc1a49fd12631a8819d9aa9e737fd45b8d516
its-mohd-kaif <mohdkaif@cedcoss.com>
2023-06-27 16:12:40 +0530

    Add Intro.js in my code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
+import introJs from "intro.js";
+import "intro.js/introjs.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  const introRef = useRef(null);
+
+  useEffect(() => {
+    const startIntro = () => {
+      introJs()
+        .setOptions({
+          steps: [
+            {
+              element: introRef.current,
+              intro: "This is the counter element.",
+            },
+            // Add more steps as needed
+          ],
+        })
+        .start();
+    };
+
+    startIntro();
+  }, []);
 
   const increase = () => {
-    setCount((prevCount) => prevCount + 1)
+    setCount((prevCount) => prevCount + 1);
   };
 
   const decrease = () => {
-    setCount((prevCount) => prevCount - 1)
+    setCount((prevCount) => prevCount - 1);
   };
 
   const reset = () => setCount(0);
@@ -17,7 +38,7 @@ function App() {
   return (
     <div className="counter">
       <p className="title">Learn Husky & Intro Js</p>
-      <p className="count">{count}</p>
+      <p className="count" ref={introRef}>{count}</p>
       <div className="controls">
         <button onClick={increase}>Increase</button>
         <button onClick={decrease}>Decrease</button>
