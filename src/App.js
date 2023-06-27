62fc1d14565cf71eaf0a5261539bf7474e64a56e
its-mohd-kaif <mohdkaif@cedcoss.com>
2023-06-27 15:17:40 +0530

    add counter code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const increase = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  const decrease = () => {
+    setCount((prevCount) => prevCount - 1);
+  };
+
+  const reset = () => setCount(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="counter">
+      <p className="title">Learn Husky & Intro Js</p>
+      <p className="count">{count}</p>
+      <div className="controls">
+        <button onClick={increase}>Increase</button>
+        <button onClick={decrease}>Decrease</button>
+        <button onClick={reset}>Reset</button>
+      </div>
     </div>
   );
 }
